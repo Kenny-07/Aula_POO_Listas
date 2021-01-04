6550c2836eeb922393cc04826338ec8497b8ba30
Alan da Costa Santos <alancostasantos07@gmail.com>
2021-01-04 10:16:35 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula_POO_Listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
